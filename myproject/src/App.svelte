496674e2d0976fb8895235147099a39e7469e166
sonecchi <sigotoyouken@gmail.com>
2024-05-05 02:01:53 +0900

    ■ package.jsonの更新 "start": "sirv public --host 0.0.0.0 --no-clear --port 5001" "rollup-plugin-svelte": "^6.1.1",
    ■ rollup.config.jsの更新
    css.write('bundle.css');

--- myproject/src/App.svelte
+++ myproject/src/App.svelte
@@ -1,42 +1,44 @@
 <script>
-	let name = 'Yoshi';
-  let beltColour = 'black';
+let name = 'Yoshi';
+let beltColour = 'black';
 
-  const handleClick = () => {
+const handleClick = () => {
     beltColour = 'orange';
-  };
+};
 
-  const handleInput = (e) => {
+const handleInput = (e) => {
     beltColour = e.target.value;
-  };
+};
 </script>
 
 <main>
-	<h1>{name}</h1>
-	<p style="color: {beltColour}">{beltColour} belt</p>
-  <button on:click={handleClick}>update belt colour</button>
-  <!-- <input type="text" on:input={handleInput} value={beltColour}> -->
-  <input type="text" bind:value={beltColour}>
+    <h1>{name}</h1>
+    <p style="color: {beltColour}">{beltColour} belt</p>
+    <button on:click={handleClick}>update belt colour</button>
+    <input type="text" on:input={handleInput} value={beltColour}>
+<!--    <input type="text" bind:value={beltColour}>-->
+
 </main>
 
+
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
